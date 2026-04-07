b29121415c5d6855a5a7dc29fe6b5fc8cf430da3
Ahmed-AbdElMoneam <ahmedabdelmoneam755@gmail.com>
2026-04-07 15:36:57 +0200

    build: updated sanity version2

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,11 +3,14 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'qn5uolcf',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
-  autoUpdates: true,
+  deployment: {
+    appId: 'raa9sbxaiavsvb4l1ra6nxkc',
+    autoUpdates: true,
+  },
 })
